d7e0c516715a4a92a3183f9a5fad4be081344333
Sergey S <5818959@gmail.com>
2019-09-13 13:42:33 +0300

    [+] Adds test endpoint logging incoming messages.

--- examples/test_endpoint.php
+++ examples/test_endpoint.php
@@ -1,6 +1,8 @@
 <?php
 
-$operation = $_POST['operation'] ?? 'send';
+define('LOG_FILE', './messages.log');
+
+$operation = $_REQUEST['operation'] ?? 'send';
 
 if ('status' === $operation) {
     echo '<?xml version="1.0"?><reply>'
@@ -14,11 +16,17 @@ if ('status' === $operation) {
 } elseif ('account' === $operation) {
     echo '<?xml version="1.0"?><reply><account>99999</account></reply>';
 } else {
+    @file_put_contents(LOG_FILE, implode(PHP_EOL, [
+        'From: ' . $_REQUEST['originator'],
+        'To: ' . $_REQUEST['phones'],
+        'Message: ' . $_REQUEST['message'],
+    ]) . PHP_EOL, FILE_APPEND);
+
     echo '<?xml version="1.0"?><reply>'
        . '<result>OK</result>'
        . '<code>0</code>'
        . '<description>queued 1 messages</description>'
-       . '<message_infos><message_info><phone>79999999999</phone><sms_id>999999999999999999</sms_id></message_info>'
+       . '<message_infos><message_info><phone>' . $_REQUEST['phones'] . '</phone><sms_id>999999999999999999</sms_id></message_info>'
        . '</message_infos></reply>';
 }
 
--- readme.md
+++ readme.md
@@ -3,3 +3,16 @@
 ## Usage
 
 See `examples/`.
+
+## Test endpoint
+
+You can run the test endpoint to test your application instead of using real
+SmsTraffic HTTP API.
+
+```
+php -S localhost:8080 -t examples/
+```
+
+Then use `http://localhost:8080/test_endpoint.php` as SmsTraffic HTTP API url.
+
+All `send` incoming messages will be logged to `examples/messages.log` file.
